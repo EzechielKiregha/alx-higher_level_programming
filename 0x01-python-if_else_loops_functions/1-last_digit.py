15efab63b01bb344d41a515d8aa49495e3592fd1
EzechielKiregha <ekiregha@gmail.com>
2024-01-05 10:20:46 +0200

    python script that check the last dist if it's less than 6 or greater than 5 or it's 0

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,11 +2,11 @@
 import random
 number = random.randint(-10000, 10000)
 last = abs(number) % 10
-if last > 5:
+if last > 5 and number > 0:
     print(f"Last digit of {number} is {last} and is greater than 5")
 elif number < 0:
     print(f"Last digit of {number} is -{last} and is less than 6 and not 0")
-elif last < 6:
-    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
-else:
+elif last == 0 or number == 0:
     print(f"Last digit of {number} is {last} and is 0")
+else:
+    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
