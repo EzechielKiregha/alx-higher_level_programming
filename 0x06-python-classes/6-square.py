be583d7dc56bd66de9898f0b13393dc21d151f2d
EzechielKiregha <ekiregha@gmail.com>
2024-01-25 00:21:31 +0200

    Position must be a tuple of 2 integers

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -48,7 +48,9 @@ class Square:
         if self.__size == 0:
             print()
             return
-        [print("") for i in range(self.__position[1]]
+
+        [print("") for i in range(self.__position[1])]
         for _ in range(self.__size):
             [print(" ", end="") for j in range(self.__position[0])]
             [print("#", end="") for k in range(self.__size)]
+            print()
