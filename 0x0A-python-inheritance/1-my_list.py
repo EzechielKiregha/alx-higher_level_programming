bcd8ac09b4c5fad479ff37f967d00a64afa5b8a5
EzechielKiregha <ekiregha@gmail.com>
2024-02-06 03:30:30 -0800

    Inheriting the list class

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""in this module the my_list class inherit the List class"""
+
+class MyList(list):
+    """It's subclass of the list class"""
+
+    def __init__(self):
+        """We initialize the object attributes"""
+        super().__init__()
+
+    def print_sorted(self):
+        """prints the sorted list out"""
+        print(sorted(self))
