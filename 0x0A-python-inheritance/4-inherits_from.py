7093981f7bff1d5d92eafab6a2cb366f122acfb7
EzechielKiregha <ekiregha@gmail.com>
2024-02-07 15:43:58 -0800

    this class inherit from

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+this module supply the inherits_from function
+"""
+
+
+def inherits_from(obj, a_class):
+    """returns true if obj is a subclass of a_class, otherwise false"""
+    return(issubclass(type(obj), a_class) and type(obj) is not a_class)
