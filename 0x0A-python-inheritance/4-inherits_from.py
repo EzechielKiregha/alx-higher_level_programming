01175d2e882dbeaf01162ebd08cc00997739cfb3
EzechielKiregha <ekiregha@gmail.com>
2024-02-07 16:18:54 -0800

    Correct pycode style

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,6 @@ this module supply the inherits_from function
 """
 
 
-
 def inherits_from(obj, a_class):
     """returns true if obj is a subclass of a_class, otherwise false"""
     return(issubclass(type(obj), a_class) and type(obj) is not a_class)
