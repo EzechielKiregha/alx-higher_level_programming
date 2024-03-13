06e0cabf60f7ee1f1c0fb2241126fd474c2f95c9
EzechielKiregha <ekiregha@gmail.com>
2024-03-14 00:48:11 +0200

    Alter that DB before it alters you

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,6 @@
+-- this displays average temperatures by city in descending order
+
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,4 @@
+-- this computes the maximum by column of a group of records in a table in the database
+SELECT state, MAX(value) AS max_temp FROM temperatures
+    GROUP BY state
+    ORDER BY state;
