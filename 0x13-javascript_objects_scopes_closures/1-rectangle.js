bd17c21576b22cc6acc8aee78b9512ed4d96a3aa
EzechielKiregha <ekiregha@gmail.com>
2024-04-12 01:42:08 +0200

    ReCtanGle

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,12 +1,7 @@
 #!/usr/bin/node
-
-class Rectangle {
-  constructor (w, h) {
-    this.width = w;
-    this.height = h;
-  }
-}
-
-module.exports = {
-  Rectangle
+module.exports = class Rectangle {
+	constructor (w, h) {
+		this.width = w;
+		this.height = h;
+	}
 };
