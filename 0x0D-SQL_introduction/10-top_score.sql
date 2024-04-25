8420b8bc9634d931804376da67b8a8e1d2756e29
EzechielKiregha <ekiregha@gmail.com>
2024-04-25 23:30:09 +0200

    Wooow merging is complicating things, I am sure when I got my hands of it, nothing will stand in my way

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,3 @@
--- Lists all records of the table second_table.
--- Records are ordered by descending score.
-SELECT `score`, `name`
-FROM `second_table`
-ORDER BY `score` DESC;
+-- this lists all records of the table second_table
+-- and displays both score and name, and database is passed as an argument of the mysql command
+SELECT score, name FROM second_table ORDER BY score DESC;
