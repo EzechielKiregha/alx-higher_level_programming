26c36129b52a5b426046cbeb07060595480ed5c4
EzechielKiregha <ekiregha@gmail.com>
2024-04-25 20:15:44 +0200

    get response content-length

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# Check if URL argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 URL"
-    exit 1
-fi
+# a script that gets the total number of bytes of a response body when using curl
+curl -s $1 | wc -c
 
-url=$1
+# # Check if URL argument is provided
+# if [ -z "$1" ]; then
+#     echo "Usage: $0 URL"
+#     exit 1
+# fi
 
-# Send request to URL and get the size of the response body in bytes
-size=$(curl -s -o /dev/null -w "%{size_download}" "$url")
+# url=$1
 
-# Display the size of the response body
-echo "$size"
+# # Send request to URL and get the size of the response body in bytes
+# size=$(curl -s -o /dev/null -w "%{size_download}" "$url")
+
+# # Display the size of the response body
+# echo "$size"
