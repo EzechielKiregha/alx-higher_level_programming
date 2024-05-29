8d821af747582bd9645b83facaee77f86db7ef2b
EzechielKiregha <ekiregha@gmail.com>
2024-05-30 00:12:51 +0200

    Javascript DOMContentLoaded

--- 0x15-javascript-web_jquery/100-script.js
+++ 0x15-javascript-web_jquery/100-script.js
@@ -1,4 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const el = document.querySelector('header');
-  el.style.color = '#FF0000';
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('header').style.color = '#FF0000';
 });
